5ff9923c5abd7948fa23de06d087d95108fd3672
Jessie J Valladares <shajessie@me.com>
2020-04-19 02:07:35 -0400

    Refactoring...

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {Course} from '../interfaces/course';
-import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
-import {CoursesService} from '../services/courses.service';
-
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../interfaces/course';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { CoursesService } from '../services/courses.service';
 
 @Component({
     selector: 'home',
@@ -11,42 +10,29 @@ import {CoursesService} from '../services/courses.service';
     styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  public courses$: Observable<Course[]>;
+  public beginnersCourses$: Observable<Course[]>;
+  public advancedCourses$: Observable<Course[]>;
+  public processingOngoing = false;
 
-    courses$: Observable<Course[]>;
+    constructor(private coursesService: CoursesService) {}
 
-    beginnersCourses$: Observable<Course[]>;
+  public ngOnInit() {
+    this.reloadCourses();
+  }
 
-    advancedCourses$: Observable<Course[]>;
+  public reloadCourses() {
+    this.courses$ = this.coursesService.loadAllCourses();
+    this.beginnersCourses$ = this.courses$.pipe(
+        map(courses => courses.filter(
+          course => course.categories.includes('BEGINNER'))));
+    this.advancedCourses$ = this.courses$.pipe(
+        map(courses => courses.filter(
+            course => course.categories.includes('ADVANCED'))));
+  }
 
-    processingOngoing = false;
-
-    constructor(
-      private coursesService: CoursesService) {
-
-    }
-
-    ngOnInit() {
-
-        this.reloadCourses();
-
-    }
-
-    reloadCourses() {
-
-        this.courses$ = this.coursesService.loadAllCourses();
-
-        this.beginnersCourses$ = this.courses$.pipe(
-            map(courses => courses.filter(
-              course => course.categories.includes('BEGINNER'))));
-
-        this.advancedCourses$ = this.courses$.pipe(
-            map(courses => courses.filter(
-                course => course.categories.includes('ADVANCED'))));
-    }
-
-
-  subscribeToPlan() {
-
+  public subscribeToPlan() {
+    
   }
 
 }
